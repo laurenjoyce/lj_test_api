e8feab173003af4f78d948f9a61d0e3b872c2578
Lauren Joyce <joycelc4186@gmail.com>
2017-03-02 01:56:56 -0600

    testing

--- lib/lj_test_api.rb
+++ lib/lj_test_api.rb
@@ -6,8 +6,31 @@ module LjTestApi
   class ApiCall
     def self.url(api_endpoint)
       data = Unirest.get("#{api_endpoint}").body
-      return :json => data
+
+      # json.array! data.each do |object|
+      #   json.department object["department"]
+      #   json.employee_annual_salary object["employee_annual_salary"]
+      #   json.job_titles object["job_titles"]
+      #   json.name object["name"]
+      # end
+      data.each do |object|
+        return :json => object
+      end
     end
+# json.first_name employee.first_name
+# json.last_name employee.last_name
+# json.full_name employee.full_name
+# json.birthdate employee.birthdate
+# json.email employee.email
+# json.ssn employee.ssn
+
+
+# {"department"=>"POLICE", 
+#   "employee_annual_salary"=>"129096.00",
+#  "job_titles"=>"ASST GENERAL COUNSEL", 
+#   "name"=>"BAZAREK, WILLIAM E"}
+
+
 
     # def self.department(parameter)
     #   bulk_data = Unirest.get("https://data.cityofchicago.org/resource/tt4n-kn4t.json?department=#{parameter.upcase}").body
