e340bd85fa2b23e6f11e9aa13f23e6351a7e3af6
Lauren Joyce <joycelc4186@gmail.com>
2017-03-01 21:54:24 -0600

    added unirest call

--- lib/lj_test_api.rb
+++ lib/lj_test_api.rb
@@ -1,5 +1,10 @@
 require "lj_test_api/version"
+require "unirest"
 
 module LjTestApi
-  # Your code goes here...
+  class Employee
+    def self.all
+      bulk_data = Unirest.get("https://data.cityofchicago.org/resource/tt4n-kn4t.json").body
+      bulk_data
+  end
 end
